849b565e27284e9b9bcd45dd5ad7846d6fb7d42b
NTD1993 <nolan.t.desouza@gmail.com>
2020-01-16 16:06:24 -0500

    Committing .gitignore

---
+++ src/studentlist/test.java
@@ -0,0 +1,18 @@
+   /*
+        [Class name]
+        Author: [the name of the person who created the class. It might be different from yours]
+        Date: [date]
+
+        Description
+        [Description about the class]
+    */
+
+package studentlist;
+
+/**
+ *
+ * @author nolan
+ */
+public class test {
+
+}
